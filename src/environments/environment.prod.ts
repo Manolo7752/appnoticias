c354ee30d32c4c3d6eb3bf8ff8b9feee67f48d73
Manolo7752 <Manol@LAPTOP-V2UUO8E8>
2022-09-03 15:18:38 -0400

    03.09.2022 Entrega

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiKey:'a72c0a6c0a834f82bdfa9b0229164630'
+  apiKey:'a72c0a6c0a834f82bdfa9b0229164630',
+  url: 'https://newsapi.org/v2'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiKey:'a72c0a6c0a834f82bdfa9b0229164630'
+  apiKey:'a72c0a6c0a834f82bdfa9b0229164630',
+  url:'https://newsapi.org/v2'
 };
 
 /*
